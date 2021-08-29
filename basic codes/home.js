42f5cb19bca5fb970eb260e4c82ce07bc045b277
wsna <wsna524@gmail.com>
2021-08-29 15:18:48 +0530

    Js mosh

---
+++ basic codes/home.js
@@ -0,0 +1,23 @@
+console.log("hello");
+
+//alert('yoo');
+prompt('agrr');
+
+function fun(){
+    alert('function');
+}
+
+fun();
+
+function name1(n){
+console.log('my name is'+n);
+}
+name1('wasana');
+
+function greet(){
+    var name=prompt('what is your name?');
+    //string concatination
+    console.log('hello'+' '+name);
+}
+
+greet();
---
+++ basic codes/index.js
@@ -0,0 +1,40 @@
+console.log("hello");
+
+var num=0;
+/*while(num<100){
+    num+=1;
+    console.log(num);
+}*/
+
+/*for(let nm=0;nm<9;nm++){
+console.log(nm);
+
+}*/
+
+
+//js Objects
+let person={
+
+    name:"wasana",
+    age :"25"
+};
+
+
+person['name']="Thilini";
+person.name = "John";
+
+console.log(person.name)
+
+//js arrays
+
+let colours = ['red','blue'];
+colours[2] = 'green';
+console.log(colours);
+console.log(colours.length);
+console.log(colours[1]);
+
+
+
+
+
+
